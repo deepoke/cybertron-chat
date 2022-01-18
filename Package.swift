85113fa2e960b4ecf483e4090f40384bf3191bff
Deepak <deepak@airtime.com>
2022-01-18 14:33:42 -0500

    Update smp package file

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "cybertron",
-            url: "https://github.com/deepoke/cybertron-chat/blob/main/cybertron-1.0.17.zip",
+            url: "https://github.com/deepoke/cybertron-chat/releases/download/1.0.17/cybertron-1.0.17.zip",
             checksum: "2a24e0b919d6ef84a9f62aa7c90350c6b30e388dba79e1358d4ea1c1afe66100"
         ),
     ]
